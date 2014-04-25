5683624422430622c6b0df927e650ca88f3aa9ef
Kristofer S. Escobar <kescobar@precisioncare.com>
2014-04-25 17:39:15 -0400

    semi-working.

--- app/js/app.js
+++ app/js/app.js
@@ -26,27 +26,54 @@ pcApp.config(['$routeProvider',
 ]);
 
 // Directive to set up time-picker control.
-pcApp.directive('timePicker', function () {
+pcApp.directive('timePicker', function ($timeout) {
     return {
       restrict: 'A',
       template: '<div class="time-group col-sm-6 col-md-3" ng-if="attendance.duration !== 0">' +
                     '<div class="input-group" ng-if="attendance.start_time">' +
                         '<span class="input-group-addon time-label">Time In:</span>' +
                         '<span class="bootstrap-timepicker">' +
-                            '<input type="text" class="timepicker form-control" value="{{attendance.start_time}}">' +
+                            '<input type="text" class="timepicker form-control" ng-model="attendance.start_time">' +
                         '</span>' +
                     '</div>' +
                     '<div class="input-group" ng-if="attendance.start_time && attendance.end_time">' +
                         '<span class="input-group-addon time-label">Time Out:</span>' +
                         '<span class="bootstrap-timepicker">' +
-                            '<input type="text" class="timepicker form-control" value="{{attendance.end_time}}">' +
+                            '<input type="text" class="timepicker form-control" ng-model="attendance.end_time">' +
                         '</span>' +
                     '</div>' +
                 '</div>',
       scope: {
-        attendance: '='
+        attendance: '=',
+        count: '@'
       },
       link: function (scope, elem, attrs) {
+
+        console.log(elem);
+
+        var initTimePicker = function(){
+            $timeout(function(){
+                $(".timepicker.form-control").each(function() {
+                    $(this).timepicker({
+                        minuteStep: 1,
+                        showInputs: false,
+                        disableFocus: true,
+                        disableMouseWheel: true
+                    });
+                });
+            }, 0);
+        };
+
+        initTimePicker();
+
+        elem.on('change', function() {
+            initTimePicker();
+        });
+
+        // scope.$watch(elem, function(value) {
+        //     initTimePicker();
+        // });
+
       }
   }
 });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -77,7 +77,15 @@ pcControllers.controller('AttendanceCtrl', ['$scope', '$http', '$routeParams',
           'Title': 'Attendance Criteria',
           'Items': ['Service', 'Date']
       };
+  $scope.hstep = 1;
+  $scope.mstep = 15;
 
+  $scope.options = {
+    hstep: [1, 2, 3],
+    mstep: [1, 5, 10, 15, 25, 30]
+  };
+
+  $scope.ismeridian = true;
       $scope.ShowTimeIn = function(attendance){
 
         /*
